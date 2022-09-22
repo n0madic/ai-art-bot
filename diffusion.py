7c2267b432c6c6e000bb39a751ca7ebb98eff664
Alexander Sheiko <alexander.sheiko@mgid.com>
2022-09-22 13:26:19 +0300

    Use k-lms scheduler

--- diffusion.py
+++ diffusion.py
@@ -1,10 +1,11 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline,LMSDiscreteScheduler
 import torch
 import sys
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 sd_model_id = 'CompVis/stable-diffusion-v1-4'
 
+scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
 pipe = StableDiffusionPipeline.from_pretrained(sd_model_id, use_auth_token=True)
 pipe.safety_checker = lambda images, **kwargs: (images, False)
 pipe.to(device)
@@ -19,6 +20,7 @@ def generate(prompt, count=1, scale=7.5, steps=50, seed=None):
                 num_inference_steps=steps,
                 guidance_scale=scale,
                 generator=generator,
+                scheduler=scheduler
             )
     else:
         images_list = pipe(
@@ -26,6 +28,7 @@ def generate(prompt, count=1, scale=7.5, steps=50, seed=None):
             num_inference_steps=steps,
             guidance_scale=scale,
             generator=generator,
+            scheduler=scheduler
         )
     images = []
     for image in images_list["sample"]:
